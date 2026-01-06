3ac2e6ddd515d758351aa6a581a01cc17cbf4b2b
Couguar <ingluemlp@gmail.com>
2026-01-06 14:16:53 +0300

    Make some userSettings

--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -3,10 +3,31 @@
 {
   programs.vscode = {
     enable = true;
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      dracula-theme.theme-dracula
-      yzhang.markdown-all-in-one
-      jnoortheen.nix-ide
-    ];
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        dracula-theme.theme-dracula
+        yzhang.markdown-all-in-one
+        jnoortheen.nix-ide
+        charliermarsh.ruff
+        vue.volar
+      ];
+      userSettings = {
+        "[nix]"."editor.tabSize" = 2;
+        "[lua]"."editor.tabSize" = 2;
+        "[python]" = {
+          "editor.formatOnSave" = true;
+          "editor.CodeActionsOnSave" = {
+            "source.fixAll" = "explicit";
+            "source.organizeImports" = "explicit";
+          };
+          "editor.defaultFormatter" = "charliermarsh.ruff";
+        };
+        "workbench"."colorTheme" = "Dracula Theme";
+        "editor" = {
+          "fontFamily" = "'JetBrains Mono', 'monospace', monospace";
+          "fontLigatures" = true;
+        };
+      };
+    };
   };
 }
