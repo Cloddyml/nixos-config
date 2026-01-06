5d4f536c4a1709ffa551d5a85d661fe5a29a29cb
Couguar <ingluemlp@gmail.com>
2026-01-06 14:59:09 +0300

    Configuring hyprpanel

--- modules/home-manager/desktop/hyprpanel.nix
+++ modules/home-manager/desktop/hyprpanel.nix
@@ -4,64 +4,63 @@
     settings = {
       layout = {
         bar.layouts = {
-	  "0" = {
-	    left = [ "dashboard" "workspaces" ];
-	    middle = [ "media" ];
-	    right = [ "volume" "systray" "notifications" ];
-	  };
-	};
+					"0" = {
+						left = [ "dashboard" "workspaces" ];
+						middle = [ "media" ];
+						right = [ "volume" "systray" "notifications" ];
+					};
+				};
       };
 
       bar.launcher.autoDetectIcon = true;
       bar.launcher.icon = "";
-      bar.workspaces.show_icons = true;
       bar.clock = {
         format = "%H:%M";
       };
 
       menus.clock = {
-        time = {
-	  military = true;
-	};
+				time = {
+					military = true;
+				};
 
-	weather = {
-	  enabled = false;
-	  unit = "metric";
-	  # key = ""; # API key
-	  location = "Saint Petersburg, Russian Federation";
-	};
-      };
+				weather = {
+					enabled = false;
+					unit = "metric";
+					# key = ""; # API key
+					location = "Saint Petersburg, Russian Federation";
+				};
+			};
       
       menus.dashboard = {
         directories.enabled = false;
-	stats.enable_gpu = false;
+				stats.enable_gpu = false;
 	
-	shortcuts = {
-	  left = {
-	    shortcut4 = {
-	      icon = "";
-	      command = "wofi --show drun";
-	      tooltip = "Search Apps";
-	    };
+				shortcuts = {
+					left = {
+						shortcut4 = {
+							icon = "";
+							command = "wofi --show drun";
+							tooltip = "Search Apps";
+						};
 
-	    shortcut1 = {
-	      icon = "ℤ";
-	      command = "zen";
-	      tooltip = "Zen Browser";
-	    };
+						shortcut1 = {
+							icon = "ℤ";
+							command = "zen";
+							tooltip = "Zen Browser";
+						};
 
-		shortcut3 = {
-			command = "vesktop";
-		};
-	  };
-	};
+						shortcut3 = {
+							command = "vesktop";
+						};
+					};
+				};
       };
       
       theme = {
         bar.transparent = true;
         font = {
           name = "CaskaydiaCove NF";
-	  size = "16px";
+	  			size = "16px";
         };
       };
     };
