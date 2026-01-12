01125d7389953a3f498134fb134645a68d6f0602
Couguar <ingluemlp@gmail.com>
2026-01-13 02:19:22 +0300

    Modify dual-boot

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -5,11 +5,23 @@
     loader = {
       grub = {
         enable = true;
-	device = "nodev";
-	efiSupport = true;
-	useOSProber = true;
+        device = "nodev";
+        efiSupport = true;
+        useOSProber = false;
+        extraEntries = ''
+          menuentry "Windows Boot Manager" {
+            insmod part_gpt
+            insmod fat
+            search --no-floppy --fs-uuid --set=root F24E-08A1
+            chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+          }
+        '';
       };
-      efi.canTouchEfiVariables = true;
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+
     };
 
     supportedFilesystems = [ "ntfs" ];
