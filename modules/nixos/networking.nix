172735811e2b1926f4e7b955a7f2beeb61bf97c2
Couguar <ingluemlp@gmail.com>
2026-01-16 23:45:51 +0300

    Add: throne tun works correctly with UDP protocol for discord

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -10,7 +10,19 @@
     firewall = {
       enable = true;
       allowedTCPPorts = [ 22 ];
+      trustedInterfaces = [ "throne-tun" ];
+      allowedUDPPortRanges = [
+        { from = 50000; to = 65535; }
+      ];
+      allowedUDPPorts = [
+        443
+        3478
+        3479
+        3480
+      ];
+      checkReversePath = "loose";
     };
+    nftables.enable = true;
   };
 
   boot.kernelModules = [ "tun" ];
